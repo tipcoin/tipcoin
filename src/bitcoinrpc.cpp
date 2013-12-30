b191df167ef92bbe96a974bf3f4670f20cd41590
MarkWaldron <markewaldron@gmail.com>
2013-12-30 01:32:08 -0500

    Wrong port setting
    What a waste of an hour

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -2889,7 +2889,7 @@ void ThreadRPCServer2(void* parg)
     // Try a dual IPv6/IPv4 socket, falling back to separate IPv4 and IPv6 sockets
     const bool loopback = !mapArgs.count("-rpcallowip");
     asio::ip::address bindAddress = loopback ? asio::ip::address_v6::loopback() : asio::ip::address_v6::any();
-    ip::tcp::endpoint endpoint(bindAddress, GetArg("-rpcport", 56679));
+    ip::tcp::endpoint endpoint(bindAddress, GetArg("-rpcport", 55883));
 
     boost::signals2::signal<void ()> StopRequests;
 
